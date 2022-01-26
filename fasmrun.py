08fee6391967b70a11c4ba0537bee95490adc55f
miko1112 <38104206+miko1112@users.noreply.github.com>
2022-01-26 15:35:22 +0000

    Update fasmrun.py

--- fasmrun.py
+++ fasmrun.py
@@ -1,8 +1,7 @@
 import fasmlib as fasm
 import sys
 
-def cleanprint(s):
-  print(s,end='')
+cleanprint =lambda s: print(s,end='')
 
 if sys.argv[1].split(".")[-1]=="frost": c=open(sys.argv[1]).read()
 else:
